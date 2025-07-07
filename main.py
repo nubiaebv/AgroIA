fddc08abeff2dcbb980eb25f23ff2883d0ea4ed0
Fiorella Abarca <fabarcavalverde@gmail.com>
2025-07-06 19:25:34 -0600

    Revert "Feat:add rnn orocesos"
    This reverts commit a2cc4d71ae7889b1e645db9abfe4c28cfe83361d.

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.procesador_rnn import ProcesadorDatosPapa
-
-if __name__ == "__main__":
-    # Crear instancia del procesador
-    archivo = "data/raw/ESTIM_papa_2005-2025 (1).xls"
-    procesador = ProcesadorDatosPapa(archivo)
-
-    # Procesar directamente a formato largo y exportar
-    procesador.procesar_y_exportar("data/processed/datos_transformados.csv")
